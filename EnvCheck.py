67ade18bb96c6f699ab2c8e58d53949788c21f5e
steve <steve@steves-MacBook-Pro.local>
2024-12-05 08:22:53 -0700

    Feature: misc

--- EnvCheck.py
+++ EnvCheck.py
@@ -1,20 +1,8 @@
 import sys
 import os
-import pprint
 
 if sys.prefix != sys.base_prefix:
    print("##Virtual##")
 else:
    print("NOT Virtual")
-
-def print_env_vars():
-  """Prints environment variables in a tabular format."""
-  env_vars = os.environ.items()
-  max_key_len = max(len(key) for key, _ in env_vars)
-
-  for key, value in env_vars:
-    print(f"{key:{max_key_len}} | {value}")
-
-if __name__ == "__main__":
-  print_env_vars()
 
